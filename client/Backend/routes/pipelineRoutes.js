93bd76f4e2ab58e9be6393ee63652ac90f812010
Samavia Arshad <meits5155@gmail.com>
2026-07-07 10:12:04 +0500

    fix: cleaned folder structure and moved frontend files to client

--- client/Backend/routes/pipelineRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const pipelineController = require("../controllers/pipelineController");
-const { protect } = require("../middlewares/authMiddleware");
-
-router.post("/move", protect, pipelineController.moveInvestor);
-router.post("/", protect, pipelineController.createPipeline);
-router.get("/", protect, pipelineController.getPipelines);
-module.exports = router;
