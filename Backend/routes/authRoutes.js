1062d4ec5198057bbcb3e4df2e3e85f5dafb0faa
Samavia Arshad <meits5155@gmail.com>
2026-07-17 16:47:02 +0500

    feat: added forgot password logic without comments

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -4,12 +4,10 @@ const router = express.Router();
 const authController = require("../controllers/authController"); 
 const { protect } = require("../middlewares/authMiddleware"); 
 
-
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.post("/forgot-password", authController.forgotPassword);
 router.get("/all-users", protect, authController.getAllUsers);
-
-
 
 router.post("/add-member", protect, authController.addMember);
 
