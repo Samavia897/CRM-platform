a54ffbef9367a851685fb9a289722f4e241ddeaa
Samavia Arshad <meits5155@gmail.com>
2026-07-19 17:24:16 +0500

    fix: corrected invalid axios import package string to fix render build crash

--- client/src/pages/Funds.jsx
+++ client/src/pages/Funds.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "react-axios";
+import axios from "axios";
 import Swal from 'sweetalert2';
 import { HiPlus, HiX, HiLocationMarker, HiDownload, HiPencilAlt, HiTrash, HiSearch, HiOfficeBuilding } from "react-icons/hi";
 
